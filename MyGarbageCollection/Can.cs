0bac7af868d0ffe0a7f143139ee3f0ff3ade4c14
Ksusha Osipova <besedinaksenya@mail.ru>
2022-10-17 21:53:07 +0400

    Add project files.

---
+++ MyGarbageCollection/Can.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGarbageCollection
+{
+    public class Can : IDisposable
+    {
+        string name;
+
+        public Can(string name)
+        {
+            this.name = name;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Throw away the can {name}");   
+        }
+    }
+}
---
+++ MyGarbageCollection/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyGarbageCollection;
+using System;
+using System.Diagnostics;
+using System.Xml.Schema;
+
+Can pepsi = new("Pepsi");
+
+pepsi.Dispose();
+
+using (Can cola = new("Cola"))
+{ 
+
+}
+
+//long[] bigArray = new long[1000000]; 
+
+long managedMemory = GC.GetTotalMemory(false);
+Console.WriteLine($"Managed memory is: {managedMemory}");
+
+using (Process proc = Process.GetCurrentProcess())
+{
+    long totalMemory = proc.PrivateMemorySize64;
+    Console.WriteLine($"Total memory is: {totalMemory}");
+    long unmanagedMemory = totalMemory - managedMemory;
+    Console.WriteLine($"Unmanaged memory is: {unmanagedMemory}");
+}
+
+
+
+
+
+
